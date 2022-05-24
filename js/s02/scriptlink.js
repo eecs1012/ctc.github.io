73092debb7a7a784fab047396794ce3d0ba7f780
eecs1012 <105894051+eecs1012@users.noreply.github.com>
2022-05-24 16:27:50 -0400

    add all js files

---
+++ js/s02/scriptlink.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function scriptlink(){
+	let solscriptlink = document.getElementById("solscriptlink");
+	
+	solscriptlink.innerHTML = "&lt;script src=\"./js/s02/blasting.js\"&gt;&lt;/script&gt;";
+}
+
---
+++ js/s02/soldom.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function soldom(){
+	let soldom = document.getElementById("soldom");
+	soldom.innerHTML = "html file maps to the document, element maps to the object<br>";
+	soldom.innerHTML += "The DOM views an HTML document as a tree of nodes. A node represents an HTML element.";
+
+}
