833263673164f1ad331a09630a3e742b73f0cc07
eecs1012 <105894051+eecs1012@users.noreply.github.com>
2022-05-31 19:55:07 -0400

    Add files via upload

---
+++ js/sctc1/soltablediff.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function soltablediff(){
+	var box = document.getElementsByClassName("correct");
+
+	for(var i = 0; i < box.length; i++){
+	    box[i].style.outline="1px solid green";
+	}
+}
+
+/*
+
+		*/
