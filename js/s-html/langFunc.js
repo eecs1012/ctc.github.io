5312ae1247a1e9793140f7258cc42e247272c18c
eecs1012 <105894051+eecs1012@users.noreply.github.com>
2022-06-13 09:31:30 -0400

    upload js functions for s-html

---
+++ js/s-html/langFunc.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function langFunc(){
+	let answer;
+	answer = document.getElementById('lanfunctionality');
+
+	answer.innerHTML = "The lang attribute specifies the language of the element content.<br> And can communicate with the search engine.";
+
+};
