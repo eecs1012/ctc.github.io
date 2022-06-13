6e768dc8049e63c63e607af0bd727b7ded6e896e
eecs1012 <105894051+eecs1012@users.noreply.github.com>
2022-06-13 09:32:03 -0400

    Delete js/s01 directory

--- js/s01/epiphanylang.js
+++
@@ -1,7 +0,0 @@
-// JavaScript source code
-function epiphanylang(){
-	let eptitle = document.getElementById('eptitle');
-	eptitle.innerHTML = "Why Google Ignores HTML Lang Attribute for Indexing in Search Results";
-	let epcontent = document.getElementById('epcontent');
-	epcontent.innerHTML= "John Mueller from Google explained that one of the problems was that people were copying code or templates that included the \"lang=\" tag, but with the wrong information, and the site owner wasn not updating it. So it turned out to be a pretty useless tag for them to use."
-};
--- js/s01/inlineblockdiff.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-function inlineblockdiff(){
-    let ansblock = document.getElementById('solblock');
-	ansblock.innerHTML = " Block elements always start in a new line and have top and bottom margins. It does not contain any other elements next to it.";
-	let ansinline = document.getElementById('solinline');
-	ansinline.innerHTML= " Inline elements occupy only enough width that is sufficient to it and allows other elements next to it which are inline. <br>Inline elements do not start from a new line and do not have top and bottom margins as block elements have."
-	let ansimg.innerHTML=
-			<p class="sol" id="solinlineblockimg"></p>
-};
